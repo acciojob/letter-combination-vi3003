63d25f147df5507c4e99360d983e9151519c3d23
“vi3003” <vidhijain3003@gmail.com>
2023-01-15 23:37:01 +0530

    first commit

--- solution.js
+++ solution.js
@@ -1,5 +1,28 @@
 function letterCombinations(input_digit) {
   //Complete the function
+  let numArr=input_digit.split('');
+  let comboArr=[];
+  comboArr=combinations(comboArr,numArr);
+  return comboArr;
+}
+
+function combinations(arr,numArr){
+  let lettersArr=['0','1','abc','def','ghi','jkl','mno','pqrs','tuv','wxyz'];
+  if(numArr.length==0)  return arr;
+  let num=Number(numArr[0]);
+  numArr.shift();
+  let newArr=[];
+  if(arr.length==0){
+    newArr=lettersArr[num].split("");;
+  }else{
+    for(let i=0;i<arr.length;i++){
+      let letterArr=lettersArr[num].split("");
+      for(let j=0;j<letterArr.length;j++){
+        newArr.push(arr[i]+letterArr[j]);
+      }
+    }
+  }
+  return combinations(newArr,numArr);
 }
 
 module.exports = letterCombinations;
